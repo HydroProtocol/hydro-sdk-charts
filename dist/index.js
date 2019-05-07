e7ba1da66a090ffaa558169135da19b52dae6916
wanglei <wanglei@ddex.io>
2019-05-07 20:32:19 +0800

    ignore dist

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = __importDefault(require("./TradeChart/index"));
-exports.TradeChart = index_1.default;
-var index_2 = __importDefault(require("./DeepChart/index"));
-exports.DeepChart = index_2.default;
--- dist/lib/utils.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.capitalizeFirstLetter = function (str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-};
